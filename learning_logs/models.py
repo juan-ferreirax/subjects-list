e7e38608f7cf14d2386b7c5a3b0f20b8bf6a82ff
Juan Pablo <juanpablofc8.0@gmail.com>
2026-01-26 02:57:42 -0300

    chore: adição do TYPE_CHECKING

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from django.db.models.manager import RelatedManager
 
 # Create your models here.
 class Topic(models.Model):
     """Um assunto sobre o qual o usuário está aprendendo"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    if TYPE_CHECKING:
+        subject_set: "RelatedManager[Subject]"
 
     def __str__(self):
         """Devolve uma representação em string do modelo."""
